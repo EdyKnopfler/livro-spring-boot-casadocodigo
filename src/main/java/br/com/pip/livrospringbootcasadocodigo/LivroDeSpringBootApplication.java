15397ded778892bd015a1515dd6ba680a3fbf477
Éderson Cássio <ederson.cassio@scipopulis.com>
2021-06-08 20:59:37 -0300

    Setup inicial

---
+++ src/main/java/br/com/pip/livrospringbootcasadocodigo/LivroDeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.pip.livrospringbootcasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroDeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroDeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pip/livrospringbootcasadocodigo/LivroDeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pip.livrospringbootcasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroDeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
